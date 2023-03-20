8007a086958ed29a7a80f28ac005d7493042ad80
Pradeep Makone <pradeepmakone@gmail.com>
2023-03-20 20:39:21 +0530

    issue in progress test

--- src/includes/issue-in-progress.php
+++ src/includes/issue-in-progress.php
@@ -2,22 +2,7 @@
 $curl->post(
 	'https://api.github.com/graphql',
 	array(
-		'query' => 'mutation {
-			updateProjectV2ItemFieldValue(
-				input: {
-					projectId: "PVT_kwDOB6B2784ANa0H"
-					itemId: "PVTI_lADOB6B2784ANa0HzgFhigM"
-					fieldId: "PVTSSF_lADOB6B2784ANa0HzgIkEpo"
-					value: { 
-						singleSelectOptionId: "fae3adc8"
-					}
-				}
-			) {
-				projectV2Item {
-					id
-				}
-			}
-		}',
+		'query' => 'mutation { updateProjectV2ItemFieldValue( input: { projectId: "PVT_kwDOB6B2784ANa0H" itemId: "PVTI_lADOB6B2784ANa0HzgFhigM" fieldId: "PVTSSF_lADOB6B2784ANa0HzgIkEpo" value: { singleSelectOptionId: "fae3adc8" } } ) { projectV2Item { id } } }',
 	)
 );
 
