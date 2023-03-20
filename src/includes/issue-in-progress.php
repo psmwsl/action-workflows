9dbca28ad240a6fe735905e34d889cda0cf787f1
Pradeep Makone <pradeepmakone@gmail.com>
2023-03-20 20:49:11 +0530

    issue in progress test

--- src/includes/issue-in-progress.php
+++ src/includes/issue-in-progress.php
@@ -1,15 +1,44 @@
 <?php
+// $curl->post(
+// 	'https://api.github.com/graphql',
+// 	array(
+// 		'query' => 'mutation { updateProjectV2ItemFieldValue(
+// 				input: {
+// 					projectId: "PVT_kwDOB6B2784ANa0H"
+// 					itemId: "PVTI_lADOB6B2784ANa0HzgFhigM"
+// 					fieldId: "PVTSSF_lADOB6B2784ANa0HzgIkEpo"
+// 					value: { 
+// 						singleSelectOptionId: "fae3adc8"
+// 					}
+// 				}
+// 			) {
+// 				projectV2Item {
+// 					id
+// 				}
+// 			}
+// 		}',
+// 	)
+// );
+
 $curl->post(
 	'https://api.github.com/graphql',
 	array(
-		'query' => 'mutation { updateProjectV2ItemFieldValue( input: { projectId: "PVT_kwDOB6B2784ANa0H" itemId: "PVTI_lADOB6B2784ANa0HzgFhigM" fieldId: "PVTSSF_lADOB6B2784ANa0HzgIkEpo" value: { singleSelectOptionId: "fae3adc8" } } ) { projectV2Item { id } } }',
+		'query' => 'query{
+			node(id: "PROJECT_ID") {
+				... on ProjectV2 {
+					fields(first: 20) {
+						nodes {
+							... on ProjectV2FieldCommon {
+								id
+								name
+							}
+						}
+					}
+				}
+			}
+		}',
 	)
 );
 
 var_export( $curl->getHttpStatusCode() );
-echo PHP_EOL . '--------' . PHP_EOL;
-if ( $curl->error ) {
-	var_export( $curl );
-} else {
-	var_export( $curl->response );
-}
+var_export( $curl->response );
