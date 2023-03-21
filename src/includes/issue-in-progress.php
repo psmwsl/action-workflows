a31301d54cc65167820ce6c9647741a87b9587b6
Pradeep Makone <pradeepmakone@gmail.com>
2023-03-21 05:37:09 +0530

    improvements

--- src/includes/issue-in-progress.php
+++ src/includes/issue-in-progress.php
@@ -2,7 +2,7 @@
 /**
  * Change issue item to "In Progress" whenever new branch is created for the issue
  * Environment variables available to use:
- * param1: issue number
+ * param1: branch name
  *
  * @package psmwsl
  */
@@ -11,26 +11,31 @@ use Curl\Curl;
 
 $curl = new Curl();
 PSMWSL_Functions::set_headers( $curl );
-$item_id = PSMWSL_Functions::get_project_item_id( getenv( 'param1' ) );
-echo 'Item id received';
-$curl->post(
-	'https://api.github.com/graphql',
-	array(
-		'query' => 'mutation ( $item_id: ID! ) {
-			updateProjectV2ItemFieldValue(
-				input: {
-					projectId: "PVT_kwDOB6B2784ANa0H"
-					itemId: $item_id
-					fieldId: "PVTSSF_lADOB6B2784ANa0HzgIkEpo"
-					value: { 
-						singleSelectOptionId: "fae3adc8"
+
+// retrive issue number from branch name.
+if ( preg_match( '/^(\d+)-\S+$/i', getenv( 'param1' ), $matches ) ) {
+
+	$item_id = PSMWSL_Functions::get_project_item_id( $matches[1] );
+	echo 'Item id received';
+	$curl->post(
+		'https://api.github.com/graphql',
+		array(
+			'query' => 'mutation ( $item_id: ID! ) {
+				updateProjectV2ItemFieldValue(
+					input: {
+						projectId: "PVT_kwDOB6B2784ANa0H"
+						itemId: $item_id
+						fieldId: "PVTSSF_lADOB6B2784ANa0HzgIkEpo"
+						value: { 
+							singleSelectOptionId: "fae3adc8"
+						}
+					}
+				) {
+					projectV2Item {
+						id
 					}
 				}
-			) {
-				projectV2Item {
-					id
-				}
-			}
-		} variables { "item_id": ' . $item_id . ' }',
-	)
-);
+			} variables { "item_id": ' . $item_id . ' }',
+		)
+	);
+}
